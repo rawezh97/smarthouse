f266fef93daa39c78fc04daca12ef6c0ca60ddf0
Rawe Zh <92225352+rawezh97@users.noreply.github.com>
2022-07-20 12:02:01 +0300

    Update smarthouse.py

--- smarthouse.py
+++ smarthouse.py
@@ -2,8 +2,8 @@ import Adafruit_DHT
 import time
 import RPi.GPIO as GPIO
 import RPi.GPIO as ultra
-GPIO.setmode(GPIO.BOARD)
 
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11 , GPIO.OUT)    #Tempreture  LED RED  GPIO 17
 GPIO.setup(13 , GPIO.OUT)    #Tempreture  LED GREEN GPIO 27
 GPIO.setup(37 , GPIO.OUT)   #humidity    LED  GPIO 26
@@ -11,7 +11,6 @@ GPIO.setup(32 , GPIO.OUT)   #distance    LED  GPIO 12
 
 dht11_sensor = Adafruit_DHT.DHT11
 dht11_pin = 23  #GPIO 23         #DHT11 input
-
 trig = 18   #GPIO 24
 echo = 22   #GPIO 25
 GPIO.setup(trig , GPIO.OUT)
@@ -40,7 +39,7 @@ while True:
         while GPIO.input(echo) == 1:
             end_pulse = time.time()
         pluse_duration = end_pulse - start_pulse
-        distance = round((pluse_duration * 34000)/2)
+        distance = round((pluse_duration * 34300)/2)
         #during = round(distance , 2)
         print (f"temp = {tempreture}*C hum = {humidity}% Distance : {distance} cm ")
         if distance <50 :
